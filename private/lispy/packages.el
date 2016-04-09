2446ffcd27b22934bc11eca462f90ac10f70c198
Mika Vilpas <mika.vilpas@gmail.com>
2016-04-09 09:59:08 +0300

    Start evil-lispy-mode properly, and add opinionated key bindings

--- private/lispy/packages.el
+++ private/lispy/packages.el
@@ -3,18 +3,25 @@
     (evil-lispy :location (recipe :fetcher github
                                   :repo "sp3ctum/evil-lispy"
                                   :branch "master"))))
+
 (defun lispy/init-lispy ()
   (use-package lispy
     :defer t))
 
 (defun lispy/init-evil-lispy ()
   (use-package evil-lispy
-    :init (add-hook 'emacs-lisp-mode-hook #'evil-lispy-state-entry)
-    :defer t))
+    :defer t
+    :init (add-hook 'emacs-lisp-mode-hook #'evil-lispy-mode)
+    :commands (evil-lispy-mode)
+    :config (progn
+              (spacemacs|diminish evil-lispy-mode " Ⓛ" " L")
 
-;; todo call spacemacs|add-toggle
-;; (spacemacs|add-toggle evil-cleverparens
-;;   :status evil-cleverparens-mode
-;;   :on  (evil-cleverparens-mode)
-;;   :off (evil-cleverparens-mode -1)
-;;   :documentation "Enable evil-cleverparens.")
+              (when (configuration-layer/package-usedp 'cider)
+                ;; show eval results in a cider overlay, next to point
+                (add-to-list 'lispy-compat 'cider)
+                (setq lispy-eval-display-style 'overlay))
+
+              (define-key lispy-mode-map "o" 'special-lispy-different)
+              (define-key lispy-mode-map "d" 'special-lispy-other-mode)
+              (define-key lispy-mode-map "i" 'special-lispy-flow)
+              (define-key lispy-mode-map "f" 'special-lispy-tab))))
